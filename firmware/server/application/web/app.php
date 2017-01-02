4e7373370b16b296a2e74f253ef48b733c6544fa
Ivan Podorozhny <ivan@podorozhny.ru>
2017-01-02 06:56:16 +0300

    .

--- firmware/server/application/web/app.php
+++ firmware/server/application/web/app.php
@@ -2,16 +2,35 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Doctrine\DBAL\Connection;
 use Silex\Application;
 use Silex\ControllerCollection;
+use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 
 $app = new Application();
 
-$debug = true;
+$app['debug'] = true;
 
-$app['debug'] = $debug;
+$app->register(new DoctrineServiceProvider(), [
+    'db.options' => [
+        'driver'   => 'pdo_pgsql',
+        'host'     => 'database',
+        'dbname'   => 'bot_db',
+        'user'     => 'bot',
+        'password' => 'bot',
+        'charset'  => 'utf8',
+    ],
+]);
+
+/** @var Connection $db */
+$db = &$app['db'];
+
+$app->register(new TwigServiceProvider(), [
+    'twig.path'    => __DIR__ . '/../app/views',
+    'twig.options' => ['debug' => $app['debug']],
+]);
 
 $app->before(function (Request $request) {
     if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
@@ -19,11 +38,6 @@ $app->before(function (Request $request) {
         $request->request->replace(is_array($data) ? $data : []);
     }
 });
-
-$app->register(new TwigServiceProvider(), [
-    'twig.path'    => __DIR__ . '/../app/views',
-    'twig.options' => ['debug' => $debug],
-]);
 
 $app->get('/', function () use ($app) {
     return $app['twig']->render('pages/dashboard.html.twig');
@@ -36,17 +50,22 @@ $apiVersion1->get('/coordinates', function () use ($app) {
     return $app->json('coordinates_list', 200);
 });
 
-$apiVersion1->get('/coordinates/last/{count}', function () use ($app) {
-    return $app->json(sprintf('last_%d_coordinates_list', $app->escape('count')), 200);
+$apiVersion1->get('/coordinates/last/{count}', function ($count) use ($app) {
+    return $app->json(sprintf('last_%d_coordinates_list', $count), 200);
 });
 
-$apiVersion1->post('/coordinates/{deviceId}', function (Request $request, $deviceId) use ($app) {
-    $item = [
-        'device_id' => $deviceId,
-        'latitude'  => $request->request->get('latitude'),
-        'longitude' => $request->request->get('longitude'),
-    ];
+$apiVersion1->post('/coordinates/{deviceId}', function (Request $request, $deviceId) use ($app, $db) {
+    $device = $db->fetchAssoc('SELECT * FROM posts WHERE id = :id', ['id' => (int) $deviceId]);
 
+    var_dump($device);
+    die();
+
+//    $item = [
+//        'device_id' => $deviceId,
+//        'latitude'  => $request->request->get('latitude'),
+//        'longitude' => $request->request->get('longitude'),
+//    ];
+//
     $item['created_at'] = (new \DateTime())->format('c');
 
     return $app->json($item, 201);
